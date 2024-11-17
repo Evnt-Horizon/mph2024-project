baa2fafb240f613cade495d1ad4f9c2bfc07f193
MeZe <lucaschauveau007@gmail.com>
2024-11-16 20:33:31 -0500

    Added HUD data alongside a controller to remotely set the values displayed

---
+++ Assets/Scripts/HUDCotroller.cs
@@ -0,0 +1,56 @@
+using TMPro;
+using UnityEngine;
+
+public class HUDCotroller : MonoBehaviour
+{
+    public TextMeshProUGUI fpsText;
+    public TextMeshProUGUI timeText;
+    
+    public TextMeshProUGUI energyLossText;
+    public TextMeshProUGUI wavelengthText;
+    public TextMeshProUGUI timeDilationText;
+    
+    public TextMeshProUGUI posXText;
+    public TextMeshProUGUI posYText;
+    public TextMeshProUGUI posZText;
+
+    public void setFps(string fps)
+    {
+        fpsText.text = "FPS: " + fps;
+    }
+
+    public void setTime(string time)
+    {
+        timeText.text = "Time: " + time;
+    }
+
+    public void setEnergyLoss(string energyLoss)
+    {
+        energyLossText.text = "Energy Loss: " + energyLoss;
+    }
+
+    public void setWavelength(string wavelength)
+    {
+        wavelengthText.text = "Wavelength: " + wavelength;
+    }
+    
+    public void setTimeDilation(string timeDilation)
+    {
+        timeDilationText.text = "Time Dilation: " + timeDilation;
+    }
+
+    public void setPosX(string posX)
+    {
+        posXText.text = "X: " + posX;
+    }
+    
+    public void setPosY(string posY)
+    {
+        posYText.text = "Y: " + posY;
+    }
+    
+    public void setPosZ(string posZ)
+    {
+        posZText.text = "Z: " + posZ;
+    }
+}
